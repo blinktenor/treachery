46a58914f7578521280f7250882d98449d6290e5
Blink Tenor <blinktenor@gmail.com>
2023-06-26 13:23:38 -0500

    Add rotating home screen images and fix page to be better for mobile viewers

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
 'use client'
 import Image from 'next/image'
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import useUserId from '/hooks/useUserId';
 
 const Home: React.FC = () => {
   const router = useRouter();
   const userId = useUserId();
+  const [gameInputValue, setGameInputValue] = useState('');
+  const [errorCode, setErrorCode] = useState('');
+  const images = ['home1.jpg', 'home2.jpg', 'home3.jpg'];
+  const [backgroundImage, setBackgroundImage] = useState('');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
 
   const startGame = () => {
     // Generate random 8-digit ID
@@ -17,10 +25,41 @@ const Home: React.FC = () => {
     router.push(`/queue/${gameId}`);
   };
 
+  const joinGame = () => {
+    const gameIdInput = gameInputValue.trim();
+
+    if (gameIdInput && gameIdInput.length === 8) {
+      router.push(`/queue/${gameIdInput}`);
+    } else {
+      setErrorCode('Game id must be 8 characters');
+    }
+  };
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const selectedImage = images[randomIndex];
+    setBackgroundImage(selectedImage);
+  }, []);
+
+  const divStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    width: '100%',
+    height: '100vh',
+  };
+  
   return (
-    <div>
+    <div style={divStyle} className='homescreen'>
       <h1>Treachery</h1>
-      <button onClick={startGame}>Start a Game</button>
+      <div className='newGame'>
+        <button onClick={startGame}>Start a Game</button>
+      </div>
+      <div className='loadGame'>
+        <input type="text" value={gameInputValue} onChange={handleInputChange} placeholder="Enter game ID" />
+        <button onClick={joinGame}>Join A Game</button>
+        <div id='errorCode'> {errorCode} </div>
+      </div>
     </div>
   );
 };

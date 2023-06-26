9e1a8d78716d0c7a5a8db29cd877c3adfb5ef30b
Blink Tenor <blinktenor@gmail.com>
2023-06-26 11:52:14 -0500

    Add basic home page that sets a userId on the session. Need to see if this should be on local storage instead?

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
+'use client'
 import Image from 'next/image'
 
-export default function Home() {
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import useUserId from '/hooks/useUserId';
+
+const Home: React.FC = () => {
+  const router = useRouter();
+  const userId = useUserId();
+
+  const startGame = () => {
+    // Generate random 8-digit ID
+    const gameId = Math.floor(Math.random() * 100000000);
+
+    // Redirect to /queue/<id>
+    router.push(`/queue/${gameId}`);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        Treachery
-      </div>
-    </main>
-  )
-}
+    <div>
+      <h1>Treachery</h1>
+      <button onClick={startGame}>Start a Game</button>
+    </div>
+  );
+};
+
+export default Home;
---
+++ hooks/useUserId.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+
+const useUserIdStorage = (): string | null => {
+  if (typeof window === 'undefined') return;
+
+  const generateUserId = (): string => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let userId = '';
+
+    for (let i = 0; i < 8; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      userId += characters.charAt(randomIndex);
+    }
+
+    return userId;
+  };
+
+  useEffect(() => {
+    const saveUserIdToStorage = () => {
+      if (!sessionStorage.getItem('userId')) {
+        sessionStorage.setItem('userId', generateUserId);
+      }
+    };
+  }, []);
+
+  return sessionStorage.getItem('userId');
+};
+
+export default useUserIdStorage;
